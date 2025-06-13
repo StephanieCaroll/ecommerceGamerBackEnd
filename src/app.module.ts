9b4cebdb1a8f353902dac21a0012218b4f15dfbc
Stephanie <stephaniecaroline2006@gmail.com>
2025-06-12 23:24:22 -0300

    Atualizações

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,13 +13,14 @@ import { UsuariosModule } from './usuarios/usuarios.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'macacorosa',
-      database: 'ecommerce_gamer',
+      // MUDANÇAS AQUI: Use process.env.<NOME_DA_VARIAVEL>
+      host: process.env.DB_HOST, // O host do seu banco de dados
+      port: parseInt(process.env.DB_PORT || '3306', 10), // A porta do seu banco de dados, com fallback para 3306 e conversão para número
+      username: process.env.DB_USER, // O usuário do seu banco de dados
+      password: process.env.DB_PASSWORD, // A senha do seu banco de dados
+      database: process.env.DB_NAME, // O nome do seu banco de dados
       entities: [Usuario, Produto],
-      synchronize: false
+      synchronize: false // Mantenha isso como 'false' em produção!
     }),
     TypeOrmModule.forFeature([Usuario, Produto]),
     HttpModule,
